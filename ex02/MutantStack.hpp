654a79696c5a7e9526c5a3b98dcd595589c29962
Matthias Strauss <mstrauss@student.42heilbronn.de>
2025-04-11 15:13:34 +0200

    ex02: fixed typo in class. Added Testcases from subject to main.cpp

--- ex02/MutantStack.hpp
+++ ex02/MutantStack.hpp
@@ -6,7 +6,33 @@
 /*   By: mstrauss <mstrauss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/03 16:41:04 by mstrauss          #+#    #+#             */
-/*   Updated: 2025/04/03 16:41:06 by mstrauss         ###   ########.fr       */
+/*   Updated: 2025/04/11 15:12:36 by mstrauss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef MUTANTSTACK_HPP
+#define MUTANTSTACK_HPP
+#include <algorithm>
+#include <deque>
+#include <iostream>
+#include <stack>
+
+template <class T, class Container = std::deque<T>>
+
+class MutantStack : public std::stack<T, Container> {
+public:
+  MutantStack();
+  MutantStack(const MutantStack &other);
+  MutantStack &operator=(const MutantStack &other);
+  ~MutantStack();
+
+  typedef typename std::stack<T, Container>::container_type::iterator iterator;
+
+  iterator begin();
+  iterator end();
+
+private:
+};
+
+#include "MutantStack.tpp";
+#endif
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,10 +6,40 @@
 /*   By: mstrauss <mstrauss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/03 16:24:14 by mstrauss          #+#    #+#             */
-/*   Updated: 2025/04/03 16:41:43 by mstrauss         ###   ########.fr       */
+/*   Updated: 2025/04/11 15:09:39 by mstrauss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "MutantStack.hpp"
 
-int main(void) {}
+int main(void) {
+  MutantStack<int> mstack;
+
+  mstack.push(5);
+  mstack.push(17);
+
+  std::cout << mstack.top() << std::endl;
+
+  mstack.pop();
+
+  std::cout << mstack.size() << std::endl;
+
+  mstack.push(3);
+  mstack.push(5);
+  mstack.push(737);
+  //[...]
+  mstack.push(0);
+
+  MutantStack<int>::iterator it = mstack.begin();
+  MutantStack<int>::iterator ite = mstack.end();
+
+  ++it;
+  --it;
+
+  while (it != ite) {
+    std::cout << *it << std::endl;
+    ++it;
+  }
+  std::stack<int> s(mstack);
+  return 0;
+}
